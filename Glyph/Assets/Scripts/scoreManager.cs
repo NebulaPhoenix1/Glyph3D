769b5b08a364b9c4fcd9e27a955100abf50f72e2
Luke <luke.robb210@gmail.com>
2024-10-03 13:49:45 +0100

    Working Score and survival time bar

--- Glyph/Assets/Scripts/scoreManager.cs
+++ Glyph/Assets/Scripts/scoreManager.cs
@@ -8,14 +8,17 @@ public class scoreManager : MonoBehaviour
 {
     [Header("UI Element References")]
     [SerializeField] TMP_Text scoreText;
-    [SerializeField] Scrollbar survivalTime;
+    [SerializeField] Image survivalTime;
 
     private float maxTime = 3f;
     private float timeRemaining;
+    private float fillAmount;
     public float score;
     private float glyphChance = 0.05f;
 
     public UnityEvent playerDied;
+
+    /* Note to self: we can use a dictionary with an int property to keep track of which glyph is which and a bool property to track if its got or not */
 
     // Start is called before the first frame update
     void Start()
@@ -28,8 +31,15 @@ public class scoreManager : MonoBehaviour
         //Check for player death
         if(timeRemaining <= 0)
         {
+            Debug.Log("Death");
             playerDied.Invoke();
         }
+
+        //Update survival timer fill (has to be a float between 0 and 1)
+        timeRemaining = timeRemaining - Time.deltaTime;
+        fillAmount = timeRemaining/maxTime;
+        survivalTime.fillAmount = fillAmount;
+        glyphRoll();
     }
     
     public void scoreIncrease()
@@ -39,11 +49,29 @@ public class scoreManager : MonoBehaviour
         scoreText.text = "Score " + score;
         //Reset survival time and bar
         timeRemaining = maxTime;
+        //Fill amount goes from 0-1
+        survivalTime.fillAmount = 1;
     }
 
-    // Update is called once per frame
-    void Update()
+    //Function which gets called everytime score increases
+    //Has a chance of giving a glyph and the chance increases over time
+    private void glyphRoll()
     {
-        
+        float roll = Random.Range(0f, 1f);
+        //We got a glyph!
+        if (roll <= glyphChance)
+        {
+            Debug.Log("Glyph Get!");
+        }
+        //We didn't get a glyph
+        else
+        {
+            //If glyph chance is still small and we didn't get one, increase chance
+            if(glyphChance < 0.3f)
+            {
+                glyphChance += 0.0005f;
+            }
+        }
+
     }
 }
