ddc80a37672d719aff16d95bffcb8f72bdf3df07
mazycloud <mazycloud@gmail.com>
2024-10-10 11:24:50 +0100

    ccc

--- Glyph/Assets/Scripts/Swipe.cs
+++ Glyph/Assets/Scripts/Swipe.cs
@@ -2,22 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 
 public class Swipe : MonoBehaviour
 {
+    public MOve m;
     // Start is called before the first frame update
     void Start()
     {
         
     }
-    public void OnEndDrag(PointerEventData eventData)
+    private void Awake()
     {
-        Debug.Log("Press position + " + eventData.pressPosition);
-        Debug.Log("End position + " + eventData.position);
-        Vector3 dragVectorDirection = (eventData.position - eventData.pressPosition).normalized;
-        Debug.Log("norm + " + dragVectorDirection);
-        GetDragDirection(dragVectorDirection);
+        
     }
+    public void OnEnterDrag(PointerEventData eventData)
+    {
+        
+    }
+    private void OnMouseDrag()
+    {
+
+        Debug.Log("Press position + " + Input.mousePosition);
+        Debug.Log("End position + " + Input.mousePosition);
+       // Vector3 dragVectorDirection = (.position - eventData.pressPosition).normalized;
+       // Debug.Log("norm + " + dragVectorDirection);
+       // GetDragDirection(dragVectorDirection);
+
+    }
+   /// public void OnEndDrag(PointerEventData eventData)
+   /// {
+     //   Debug.Log("Press position + " + eventData.pressPosition);
+      ///  Debug.Log("End position + " + eventData.position);
+       // Vector3 dragVectorDirection = (eventData.position - eventData.pressPosition).normalized;
+      //  Debug.Log("norm + " + dragVectorDirection);
+       // GetDragDirection(dragVectorDirection);
+   // }
     private enum DraggedDirection
     {
         Up,
@@ -42,9 +62,14 @@ public class Swipe : MonoBehaviour
         Debug.Log(draggedDir);
         return draggedDir;
     }
+
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetMouseButtonDown(0))
+        {
+            m.
+        }
         
     }
 }
