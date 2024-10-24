792861eebaf3405f31352bddc2690788da60ae61
NebulaPhoenix1 <luke.robb210@gmail.com>
2024-10-24 11:39:14 +0100

    Glyph Spawning Start
    -Added function to reset JSON to be entirely false
    -New class: glyphSpawner
      Has method spawn glyph which checks remaning glyphs and picks a random one to spawn and updates JSON save accordingly.

---
+++ Glyph/Assets/glyphSpawner.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class glyphSpawner : MonoBehaviour
+{
+    [SerializeField] glyphManager manager;
+    //[SerializeField] GameObject[] glyphs;
+    List<int> remainingGlyphs;
+    float spawnChance = 0.005f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //spawnGlyph();
+    }
+
+    public void spawnGlyph()
+    {
+        //Get remaining glyphs
+        getRemainingGlyphs();
+        //If there are remaining glyphs
+        Debug.Log(remainingGlyphs.Count);
+        if(remainingGlyphs.Count > 0)
+        {
+            //Choose random remaining glyph and spawn it
+            int choice = Random.Range(0, remainingGlyphs.Count);
+            //Instantiate(glyphs[choice]);
+            manager.collectGlyph(remainingGlyphs[choice]);
+            //Update remaining glyphs list
+            getRemainingGlyphs();
+        }
+        //No remaining glyphs, send a warning
+        else
+        {
+            Debug.LogWarning("No Glyphs Remaining");
+        }
+        
+    }
+
+    private void getRemainingGlyphs()
+    {
+        remainingGlyphs = manager.glyphsNotCollected();
+    }
+}
